988d472b6f26377de06336bd7f04459b47ce45a5
nforne <mnforne@gmail.com>
2025-02-04 12:18:29 -0500

    FE - add services view and style

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,32 @@
+import {Link}  from "react-router-dom";
+import nforne from "../Home/nforne.jpg";
+import right from "../Home/right.png";
 import './Services.css';
 
 export default function Services(){
   return(
-    <>
-    <p>Services</p>
-    </>
+    <div className="home">  
+            <img src={nforne} id='svs_img' alt="../Home/nforne.jpg" />      
+            <hr />
+            <div>            
+              <pre>              
+              {`JUNIOR SOFTWARE DEVELOPER
+DEVOPS | AGILE TRAINED | ANALYTICAL`}              
+              </pre>            
+            <div><Link to="/contact"><img src={right} id='abt_rt' alt="../Home/right.png" /></Link><p>Contact Me? 🧐</p></div>
+            </div>
+            <div id="svs_txt">
+              <pre>{`SERVICES`}</pre>
+              
+              <ul>
+              <hr />
+                <li>Software Engineer - Full Stack [Web, Mobile]</li>
+                <li>Cloud Engineer and Architect [AWS]</li>
+                <li>Software Testing [QA, QC]</li>
+                <li>Data Analyst and AI</li>
+                <li>DevOps Engineer</li>
+              </ul>              
+            </div>
+      </div> 
   );
 };
